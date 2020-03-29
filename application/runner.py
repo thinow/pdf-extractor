19f038534033cde8de5d4c790367cb6dd9f246f0
Thierry Nowak <thinow@gmail.com>
2020-03-29 23:15:03 +0200

    extract word using regexp

--- application/runner.py
+++ application/runner.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 from pdfminer.high_level import extract_text
 
@@ -7,4 +8,14 @@ PAGE_NUMBER = int(sys.argv[2])
 
 text = extract_text(FILE, page_numbers=[PAGE_NUMBER])
 
-print(text)
+pattern = r'([A-Z\\|]+), d(ie|er|as)'  # TODO include accents
+flags = re.MULTILINE | re.IGNORECASE
+
+for line in text.splitlines():
+    match = re.match(pattern, line, flags)
+    if match:
+        article = 'd' + match.group(2)
+        word = match.group(1).replace('|', '')
+        print(f'{article} {word} ')
+
+# TODO exlude unexpected words : e.g. die Renten

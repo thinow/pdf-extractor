6382590c0b403a3901fb2c2196da3ffbb014b60f
Thierry Nowak <thinow@gmail.com>
2020-04-03 22:45:44 +0200

    add a sample

--- tests/extract_german_nouns/snapshots/snap_test_extractor.py
+++ tests/extract_german_nouns/snapshots/snap_test_extractor.py
@@ -7,7 +7,35 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_extract_nouns_from_sample 1'] = '''Aufbietung\tdie
+snapshots['test_extract_nouns_from_sample[sample-0.txt] 1'] = '''Ablenkung\tdie
+Ablenkungsmanöver\tdas
+Ableser\tder
+Handlung\tdie
+Ableserin\tdie
+Ablesung\tdie
+Ablieferung\tdie
+Ablöse\tdie
+Ableugnung\tdie
+Ablichtung\tdie
+Ablieferungstermin\tder
+Ablösesumme\tdie
+Ablösung\tdie
+Ablösungssumme\tdie
+Abluft\tdie
+Ablufttrockner\tder
+Abmachung\tdie
+Abmagerung\tdie
+Abmahnung\tdie
+Abmagerungskur\tdie
+Abmarsch\tder
+Abmeldung\tdie
+Abmelkwirtschaft\tdie
+Abmessung\tdie
+Abmilderung\tdie
+Abmoderation\tdie
+'''
+
+snapshots['test_extract_nouns_from_sample[sample-1.txt] 1'] = '''Aufbietung\tdie
 Aufblähung\tdie
 Aufblende\tdie
 Aufblick\tder
--- tests/extract_german_nouns/test_extractor.py
+++ tests/extract_german_nouns/test_extractor.py
@@ -1,3 +1,5 @@
+import pytest
+
 from contextlib import redirect_stdout
 from io import StringIO
 
@@ -6,15 +8,15 @@ from application.extract_german_nouns.extractor import GermanNounsExtractor
 SAMPLE_DIR = 'tests/extract_german_nouns/resources/'
 
 
-def test_extract_nouns_from_sample(snapshot):
+@pytest.mark.parametrize("file", ['sample-0.txt', 'sample-1.txt'])
+def test_extract_nouns_from_sample(snapshot, file):
     # given
     output = StringIO()
-    extractor = GermanNounsExtractor(SAMPLE_DIR + 'sample-1.txt')
+    extractor = GermanNounsExtractor(SAMPLE_DIR + file)
 
     # when
     with redirect_stdout(output):
         extractor.extract()
 
     # then
-    getvalue: str = output.getvalue()
-    snapshot.assert_match(getvalue)
+    snapshot.assert_match(output.getvalue())
